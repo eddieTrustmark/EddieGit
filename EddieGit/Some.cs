a7c810c3c1aa65357a7204908f45becd17fe5410
Eddie Lassiter <elassiter@trustmarkbenefits.com>
2021-10-02 23:11:51 -0400

    Add project files.

---
+++ EddieGit/Some.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EddieGit
+{
+    public class Some
+    {
+        private string _test = "test";
+
+        public Some()
+        {
+            _test = "1";
+        }
+    }
+}
